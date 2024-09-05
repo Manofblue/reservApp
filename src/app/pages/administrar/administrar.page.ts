e4935646ec5583a82e948df1b289efb7f01a195d
manofblue <re.ubilla@duocuc.cl>
2024-09-05 12:47:40 -0400

    crud con agregar funcionando

--- src/app/pages/administrar/administrar.page.ts
+++ src/app/pages/administrar/administrar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-administrar',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministrarPage implements OnInit {
 
-  constructor() { }
+  persona = new FormGroup({
+    rut : new FormControl('',[Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
+    nombre : new FormControl('',[Validators.required, Validators.pattern("[a-z]{3,5}")]),
+    //Si no existen las validaciones, debemos crear un metodo para crearla **TAREA
+    fecha_nacimiento : new FormControl('',[Validators.required]),
+    genero : new FormControl('',[Validators.required]),
+    tiene_equipo : new FormControl('no',[Validators.required]),
+    nombre_equipo : new FormControl('',[])
+  });
+
+  usuarios:any[]=[];
+
+  //El servicio nos permite trabajar información:
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    this.usuarios=this.usuarioService.getUsuarios();
   }
+
+  registrar(){
+    if( this.usuarioService.createUsuario(this.persona.value) ) {
+      alert("Usuario creado con éxito");
+      this.persona.reset();
+    }else{
+      alert("Error!, no se pudo crear el usuario!");
+    }
+  }
+
+  
+
+
 
 }
